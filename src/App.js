41109974d0c15ac2ca902a86810980b42247a69f
magikitty <elina.satola@gmail.com>
2022-10-18 11:09:33 +0300

    Add Home and RandomCocktail page components; add routing to pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import {Route, Switch} from "react-router-dom";
+
 import './App.css';
+import Home from "./pages/Home";
+import RandomCocktail from "./pages/RandomCocktail";
 
 function App() {
   return (
     <div className="App">
       <h1>Cocktail Collection</h1>
-      <p>Welcome!</p>
+      <Switch>
+        <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Route path="/random">
+          <RandomCocktail/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/RandomCocktail.js
@@ -0,0 +1,5 @@
+const RandomCocktail = () => {
+  return <h1>Random Cocktail</h1>
+}
+
+export default RandomCocktail;
