26502e7cef6344855c5f5478108566245b5fa8d2
magikitty <elina.satola@gmail.com>
2022-10-22 13:49:33 +0300

    Show dummy cocktails as CocktailList component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,28 @@
-import Cocktail from "../Components/Cocktail";
+import CocktailList from "../Components/CocktailList";
 
 const Home = () => {
+  const listCocktails = [
+    {
+      id: "1",
+      name: "Special Drink",
+      instructions: "Mix it.",
+      ingredients: ["4 cl vodka", "150 ml special juice"],
+      imageUrl: "https://www.thecocktaildb.com/images/media/drink/uuytrp1474039804.jpg",
+    },
+    {
+      id: "2",
+      name: "Fun Drink",
+      instructions: "Shake it.",
+      ingredients: ["4 cl vodka", "150 ml fun juice"],
+      imageUrl: "https://www.thecocktaildb.com/images/media/drink/g12lj41493069391.jpg",
+    },
+  ];
+
   return (
-   <div>
-     <h1>Home</h1>
-     <Cocktail
-       name={"Special Drink"}
-       instructions={["4 cl vodka", "150 ml special juice"]}
-       ingredients={["4 cl vodka", "150 ml special juice"]}
-       imageUrl={"https://www.thecocktaildb.com/images/media/drink/uuytrp1474039804.jpg"}
-     />
-   </div>
+    <div>
+      <h1>Home</h1>
+      <CocktailList cocktails={listCocktails}/>
+    </div>
   )
 }
 
