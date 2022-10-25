7e98365cd7302c56149a50f1602b1dcd565ea49e
magikitty <elina.satola@gmail.com>
2022-10-25 10:27:33 +0300

    Move form functionality to FetchCocktailWithForm component

---
+++ src/Components/FetchCocktailsWithForm.js
@@ -0,0 +1,109 @@
+import {useRef, useState} from "react";
+
+import CocktailList from "../Components/CocktailList";
+
+const FetchCocktailsWithForm = (props) => {
+  const [cocktails, setCocktails] = useState([]);
+  const [error, setError] = useState(null);
+  const refName = useRef("");
+  const [nameSearched, setNameSearched] = useState();
+
+  const fetchCocktail = async (event) => {
+    console.log("pressed submit");
+    // Prevent default submit behaviour to use custom behaviour
+    event.preventDefault();
+    setNameSearched(refName.current.value);
+    console.log("refName: " + refName.current.value);
+    console.log("name searched: " + nameSearched);
+
+    setError(null);
+
+    try {
+      // Wait for fetch to be resolved
+      const response = await fetch(props.urlApi + nameSearched);
+      console.log("url: " + props.urlApi + nameSearched);
+      console.log("response: " + response);
+      const data = await response.json();
+      console.log("data : " + data);
+
+      // Check for error and throw error message
+      if (!response.ok) {
+        throw new Error("Something went wrong...");
+      }
+
+      const transformedCocktails = data.drinks.map((cocktailData) => {
+        let ingredientsArray = getIngredients(cocktailData);
+
+        console.log("got id: ", cocktailData.idDrink);
+        return {
+          id: cocktailData.idDrink,
+          name: cocktailData.strDrink,
+          instructions: cocktailData.strInstructions,
+          ingredients: ingredientsArray,
+          imageUrl: cocktailData.strDrinkThumb,
+        };
+      });
+      setCocktails(transformedCocktails);
+      console.log(data);
+    } catch (err) {
+      setError(err.message);
+    }
+    refName.current.value = "";
+  };
+
+  const getIngredients = (cocktailData) => {
+    let ingredientsArray = [];
+    let ingredientNames = getArrayForKey(cocktailData, "strIngredient");
+    let ingredientAmounts = getArrayForKey(cocktailData, "strMeasure");
+
+    for (let i = 0; i < ingredientNames.length; i++) {
+      let ingredient = ingredientAmounts[i] + " " + ingredientNames[i];
+      ingredientsArray.push(ingredient);
+    }
+
+    return ingredientsArray;
+  }
+
+  // Create array of values for keys containing specified substring
+  const getArrayForKey = (cocktailData, keySubstring) => {
+    let valuesArray = [];
+
+    // Add ingredients to ingredients array based on key
+    Object.keys(cocktailData).forEach(
+      function (key) {
+        if (key.includes(keySubstring)) {
+          if (cocktailData[key] != null) {
+            valuesArray.push(cocktailData[key])
+          }
+        }
+      }
+    )
+    return valuesArray;
+  }
+
+  // Show error message or cocktail recipe
+  let content;
+  if (error) {
+    // Only show error if there is a message (error is true)
+    content = error && <p>{error}</p>;
+  } else {
+    content = <CocktailList cocktails={cocktails}/>;
+  }
+
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="name">Cocktail name: </label>
+          <textarea id="name" ref={refName}></textarea>
+        </div>
+        <button onClick={fetchCocktail} type="submit" id="search-btn">search</button>
+      </form>
+      <section>
+        {content}
+      </section>
+    </div>
+  )
+}
+
+export default FetchCocktailsWithForm;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,13 @@
-import FetchCocktails from "../Components/FetchCocktails";
-import {useRef, useState} from "react";
+import FetchCocktailsWithForm from "../Components/FetchCocktailsWithForm";
 
 const Home = () => {
-  // const apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-  const apiUrlBase = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-  const refName = useRef("");
-  const [nameSearched, setNameSearched] = useState();
-
-  const submitHandler = (event) => {
-    console.log("pressed submit");
-    // Prevent default submit behaviour to use custom behaviour
-    event.preventDefault();
-    setNameSearched(refName.current.value);
-    refName.current.value = "";
-  };
-
-  let content;
-  if (nameSearched) {
-    let apiFull = apiUrlBase + nameSearched;
-    content = <FetchCocktails urlApi={apiFull} buttonText="Search for cocktail"/>
-  } else {
-    content = <></>
-  }
+  const apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
   return (
     <div>
       <h1>Home</h1>
-
-      <form onSubmit={submitHandler}>
-        <div>
-          <label htmlFor="name">Cocktail name: </label>
-          <textarea id="name" ref={refName}></textarea>
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-
-      <p>Search for a cocktail</p>
-      {content}
+      <p>Search for a cocktail by name</p>
+      <FetchCocktailsWithForm urlApi={apiUrl}/>
     </div>
   )
 }
