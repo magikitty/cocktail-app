7889b55ea91fe121ac56a590c318e8a1901c52a9
magikitty <elina.satola@gmail.com>
2022-10-28 10:58:28 +0300

    Add page for Favourite cocktails

---
+++ src/pages/Favourites.js
@@ -0,0 +1,80 @@
+import {useEffect, useState} from "react";
+import CocktailList from "../Components/CocktailList";
+
+const Favourites = () => {
+  const [cocktails, setCocktails] = useState([]);
+  const urlDatabase = "https://cocktail-app-12a4c-default-rtdb.europe-west1.firebasedatabase.app/favourites.json";
+
+  const addFavouriteHandler = async (cocktail) => {
+    console.log("adding to favourites: " + cocktail);
+    // default method is GET, so need to specify when POST
+    const response = await fetch(
+      urlDatabase,
+      {
+        method: "POST",
+        body: JSON.stringify(cocktail),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log("POST data: " + data);
+  };
+
+  const deleteFavouriteHandler = async (cocktailId) => {
+    let urlDelete = "https://cocktail-app-12a4c-default-rtdb.europe-west1.firebasedatabase.app/favourites/" + cocktailId + ".json";
+    console.log(cocktailId);
+    // default method is GET, so need to specify when DELETE
+    const response = await fetch(
+      urlDelete,
+      {
+        method: "DELETE",
+        body: null,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log("DELETE data: " + data);
+  };
+
+  const fetchCocktailsDatabase = async () => {
+    const response = await fetch(
+      urlDatabase
+    );
+    const data = await response.json();
+
+    const fetchedCocktails = [];
+
+    for (const key in data) {
+      fetchedCocktails.push({
+        id: key,
+        name: data[key].name,
+        ingredients: data[key].ingredients,
+        instructions: data[key].instructions,
+        imageUrl: data[key].imageUrl,
+      });
+    }
+
+    setCocktails(fetchedCocktails);
+  };
+
+  // Called when component is evaluated, no need for separate fetching button
+  useEffect(() => {
+    fetchCocktailsDatabase();
+  }, [fetchCocktailsDatabase]);
+
+  return (
+    <div>
+      <h1>Favourites</h1>
+      <CocktailList
+        cocktails={cocktails}
+        deleteCocktailHandler={deleteFavouriteHandler}
+        buttonVisibility="isVisible"/>
+    </div>
+  )
+}
+
+export default Favourites;
